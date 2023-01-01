eaefaded2e6a4cfb2c468f7162fe9fa5d0749406
Tomohiro Ueki <ueki_tomohiro@cyberagent.co.jp>
2023-01-01 22:01:16 +0900

    fix localization

--- src/helpers/importer.ts
+++ src/helpers/importer.ts
@@ -5,3 +5,4 @@ export const importCSV = (filePath: string) => {};
 export const importGoogleSpreadSheet = (url: string) => {};
 
 export const importGoogleSpreadSheetWithCredential = (url: string) => {};
+// https://github.com/googleapis/google-api-nodejs-client
